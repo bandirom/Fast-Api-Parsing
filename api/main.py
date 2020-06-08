35222be1736525bc584296ce4ce499134e2ec408
bandirom <bandirom@ukr.net>
2020-06-08 15:41:37 +0300

    delete import os

--- api/main.py
+++ api/main.py
@@ -1,7 +1,6 @@
 from fastapi.staticfiles import StaticFiles
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
-import os
 from packages.parsing import ProcessingParse
 from packages.db import DB
 
